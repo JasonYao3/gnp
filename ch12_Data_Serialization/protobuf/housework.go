880953c819e15283263eb02260eb743597c330c7
Jason Yao <jasonyao105025@gmail.com>
2024-05-12 16:10:52 -0400

    add ch12_Data_Serialization

---
+++ ch12_Data_Serialization/protobuf/housework.go
@@ -0,0 +1,30 @@
+package protobuf
+
+import (
+	"io"
+
+	"github.com/JasonYao3/gnp/ch12_Data_Serialization/housework/v1"
+	"google.golang.org/protobuf/proto"
+)
+
+func Load(r io.Reader) ([]*housework.Chore, error) {
+	b, err := io.ReadAll(r)
+	if err != nil {
+		return nil, err
+	}
+
+	var chores housework.Chores
+
+	return chores.Chores, proto.Unmarshal(b, &chores)
+}
+
+func Flush(w io.Writer, chores []*housework.Chore) error {
+	b, err := proto.Marshal(&housework.Chores{Chores: chores})
+	if err != nil {
+		return err
+	}
+
+	_, err = w.Write(b)
+
+	return err
+}
